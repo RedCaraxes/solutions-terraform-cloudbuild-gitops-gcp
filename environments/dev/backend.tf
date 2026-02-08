26a0c4a29f274bfc0a34d112c7dda8766381fb79
terryqp <terryfiee2017@gmail.com>
2026-02-08 20:44:54 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = " rs-cloudbuild-tf-777"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="rs-cloudbuild-tf"
