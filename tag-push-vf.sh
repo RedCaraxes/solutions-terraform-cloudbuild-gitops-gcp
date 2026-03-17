b4f528159ebc71576609049a613d82d6efc884d1
terryqp <terryfiee2017@gmail.com>
2026-03-17 00:48:57 +0000

    Deploy cloudbuildtestnew14: rs-web-tier | Modo: --all

--- tag-push-vf.sh
+++ tag-push-vf.sh
@@ -74,8 +74,20 @@ fi
 
 # 5. Commit y Push
 if git diff --cached --quiet; then
-    echo "No hay cambios detectados. Solo se subira el Tag."
+    echo "No hay cambios locales nuevos detectados."
+    
+    # Solo mostramos info extra si es un apply y no hay cambios nuevos
+    if [ "$ACCION" == "apply" ]; then
+        echo "---------------------------------------------------------"
+        echo ">>> INFO: Verificando cambios del commit anterior (Plan):"
+        # Usamos || true para que si falla el log, el script no se detenga (por el set -e)
+        git log -1 --name-status --oneline || echo "No se pudo recuperar el historial."
+        echo "---------------------------------------------------------"
+    fi
+    
+    echo "Solo se subira el Tag."
 else
+    echo "Confirmando nuevos cambios locales..."
     git commit -m "Deploy $TICKET: $PROJECT_ID | Modo: ${MODE:-Selective}"
     git push origin $(git symbolic-ref --short HEAD)
 fi
