402a122c00fc16ea7da9058b3da171620800dd8b
terryqp <terryfiee2017@gmail.com>
2026-03-18 21:16:23 +0000

    Deploy 007: rs-web-tier | Modo: --all

--- modules/Network/main.tf
+++ modules/Network/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
   name          = var.subnetwork_name
   ip_cidr_range = var.ip_cidr_range
   region        = var.location
-  network       = google_compute_network.custom-test.id
+  network       = google_compute_network.network_vpc.id
   secondary_ip_range {
     range_name    = var.secondary_ip_range_name
     ip_cidr_range = var.secondary_ip_cidr_range
