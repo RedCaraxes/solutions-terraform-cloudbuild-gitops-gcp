f92b0563ab4100daee6d7de271137311787dac1c
terryqp <terryfiee2017@gmail.com>
2026-03-20 02:18:22 +0000

    Deploy 011: rs-web-tier | Modo: --all

--- Projects Production/rs-web-tier/main.tf
+++ Projects Production/rs-web-tier/main.tf
@@ -47,7 +47,7 @@ module "network_router" {
   for_each = local.routers
   project_id   = var.project
   region       = each.value.region
-  network_id   = module.network.network_id
+  network_id   = each.value.network_id
   router_name  = each.key
 }
 
