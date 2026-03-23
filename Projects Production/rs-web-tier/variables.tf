245e8ef643143713b4075a8cbdbd2cb8bcc9e531
terryqp <terryfiee2017@gmail.com>
2026-03-23 03:11:15 +0000

    Deploy 24: rs-web-tier | Modo: --all

--- Projects Production/rs-web-tier/variables.tf
+++ Projects Production/rs-web-tier/variables.tf
@@ -19,22 +19,4 @@ variable "project" {
   default     = "rs-app-tier-1"
 }
 
-variable "host_project_id" {
-  type = string
-}
 
-variable "service_project_id" {
-  type = string
-}
-
-variable "service_project_num" {
-  type = string
-}
-
-variable "region" {
-  type = string
-}
-
-variable "subnet_name" {
-  type = string
-}
